84e6b591a98617c131a5b949006dc076a858ba26
macmacal <mac.aleksandrowicz@gmail.com>
2024-04-15 18:50:56 +0200

    node cleanup

--- include/scene_objects_spawner/scene_objects_spawner.hpp
+++ include/scene_objects_spawner/scene_objects_spawner.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -9,31 +10,7 @@
 
 using namespace std::chrono_literals;
 
-template <>
-struct rclcpp::TypeAdapter<std::string, std_msgs::msg::String> {
-  using is_specialized = std::true_type;
-  using custom_type = std::string;
-  using ros_message_type = std_msgs::msg::String;
-
-  static void convert_to_ros_message(const custom_type& source, ros_message_type& destination) {
-    destination.data = source;
-  }
-
-  static void convert_to_custom(const ros_message_type& source, custom_type& destination) {
-    destination = source.data;
-  }
-};
-
 class SceneObjectsSpawner : public rclcpp::Node {
-  using MyAdaptedType = rclcpp::TypeAdapter<std::string, std_msgs::msg::String>;
-
 public:
-  SceneObjectsSpawner();
-
-private:
-  void timer_callback();
-
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<MyAdaptedType>::SharedPtr publisher_;
-  size_t count_;
+  SceneObjectsSpawner() : Node("SceneObjectsSpawner"){};
 };

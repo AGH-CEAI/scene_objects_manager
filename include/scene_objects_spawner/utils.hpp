f6250c9fd92bf44f0aa0609b2194b547f8811f8e
macmacal <mac.aleksandrowicz@gmail.com>
2024-04-15 18:45:46 +0200

    fixed reverse_map

--- include/scene_objects_spawner/utils.hpp
+++ include/scene_objects_spawner/utils.hpp
@@ -1,9 +1,10 @@
+#pragma once
 #include <map>
 
-template<typename K, typename V>
+template <typename K, typename V>
 static std::map<V, K> reverse_map(const std::map<K, V>& m) {
-    map<V, K> r;
-    for (const auto& kv : m)
-        r[kv.second] = kv.first;
-    return r;
+  std::map<V, K> r;
+  for (const auto& kv : m)
+    r[kv.second] = kv.first;
+  return r;
 }
